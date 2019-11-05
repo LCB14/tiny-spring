5b36e8421cfb5594248629bba72037b35eaf99ef
changbaol <1642272412@qq.com>
2019-11-06 00:20:40 +0800

    2019年11月06日00:20:38

--- src/main/java/us/codecraft/tinyioc/beans/BeanDefinitionReader.java
+++ src/main/java/us/codecraft/tinyioc/beans/BeanDefinitionReader.java
@@ -2,6 +2,9 @@ package us.codecraft.tinyioc.beans;
 
 /**
  * 从配置中读取BeanDefinition
+ *
+ * 解析 BeanDefinition 的接口。通过 loadBeanDefinitions(String) 来从一个地址加载类定义。
+ *
  * @author yihua.huang@dianping.com
  */
 public interface BeanDefinitionReader {

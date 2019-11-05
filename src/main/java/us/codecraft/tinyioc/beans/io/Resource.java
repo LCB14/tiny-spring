6ffd92c815b7b3ca829814a8f7520aef6005572d
changbaol <1642272412@qq.com>
2019-11-06 00:16:32 +0800

    2019年11月06日00:16:30

--- src/main/java/us/codecraft/tinyioc/beans/io/Resource.java
+++ src/main/java/us/codecraft/tinyioc/beans/io/Resource.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 /**
  * Resource是spring内部定位资源的接口。
  * @author yihua.huang@dianping.com
+ *
+ * 接口，标识一个外部资源。通过 getInputStream() 方法 获取资源的输入流 。
  */
 public interface Resource {
 
--- src/main/java/us/codecraft/tinyioc/beans/io/ResourceLoader.java
+++ src/main/java/us/codecraft/tinyioc/beans/io/ResourceLoader.java
@@ -4,10 +4,15 @@ import java.net.URL;
 
 /**
  * @author yihua.huang@dianping.com
+ * <p>
+ * 资源加载类。通过 getResource(String) 方法获取一个 Resouce 对象，是 获取 Resouce 的主要途径 。
+ * <p>
+ * 注： 这里在设计上有一定的问题，ResourceLoader 直接返回了一个 UrlResource，更好的方法是声明一个 ResourceLoader 接口，
+ * 再实现一个 UrlResourceLoader 类用于加载 UrlResource。
  */
 public class ResourceLoader {
 
-    public Resource getResource(String location){
+    public Resource getResource(String location) {
         URL resource = this.getClass().getClassLoader().getResource(location);
         return new UrlResource(resource);
     }

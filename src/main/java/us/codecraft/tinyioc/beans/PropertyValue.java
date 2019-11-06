c8ca18e4448764dc71a9a7bf820c8d213edb8fa0
changbaol <changbaol@tujia.com>
2019-11-06 19:35:48 +0800

    2019年11月06日19:35:47

--- src/main/java/us/codecraft/tinyioc/beans/PropertyValue.java
+++ src/main/java/us/codecraft/tinyioc/beans/PropertyValue.java
@@ -6,8 +6,14 @@ package us.codecraft.tinyioc.beans;
  */
 public class PropertyValue {
 
+    /**
+     * 属性名
+     */
     private final String name;
 
+    /**
+     * 属性值
+     */
     private final Object value;
 
     public PropertyValue(String name, Object value) {
